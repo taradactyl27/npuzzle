541821d4fcdc4702cb39c5d718b6b18a6c4b23d0
taradactyl27 <taradactyl27@DESKTOP-DI33A07.localdomain>
2020-02-28 13:31:35 -0500

    seperated into state class

--- solver.py
+++ solver.py
@@ -1,131 +1,58 @@
 import math
 import collections
+import state
 
-def breadth_first_search(state):
+def breadth_first_search(stateInitial):
     #TODO Improve data structures
     frontier = collections.deque()
-    explored = collections.deque()
-    frontier.append(state)
-    nodesExpanded = -1
+    explored = set()
+    initstate = state.State(stateInitial)
+    frontier.append(initstate)
+    explored.add(initstate)
+    nodesExpanded = 0
     search_depth = 0
     max_search_depth = 0
     #TODO Keep trac
     while(frontier):
         currentS = frontier.popleft()
-        #print("State:",currentS)
+        #print("State:",currentS.currentState)
         #print("Frontier:",frontier)
-        if(currentS == [0,1,2,3,4,5,6,7,8]):
+        if(currentS.solved):
             print("Goal Found!")
             print("Nodes Expanded: ",nodesExpanded)
             return
         nodesExpanded += 1
-        #TODO UNNECESSARILY ------ Keep track of 0 index for improved efficiency over the long run
-        indexOfEmpty = currentS.index(0)
-        #print(indexOfEmpty)
-        bounds = boundaries(indexOfEmpty)
-        #print(bounds)
-        if(indexOfEmpty - 3 >= 0):
-            #print("SLIDING UP")
-            #TODO Better representation of state to increase efficiency
-            tempS = list(currentS)
-            tempS[indexOfEmpty],tempS[indexOfEmpty-3] = tempS[indexOfEmpty-3], tempS[indexOfEmpty]
-            #print(tempS)
-            if not(tempS in explored):
-                frontier.append(tempS)
-                #print(frontier)
-                explored.append(tempS)
-        if(indexOfEmpty + 3 <= 8):
-            #print("SLIDING DOWN")
-            tempS = list(currentS)
-            tempS[indexOfEmpty],tempS[indexOfEmpty+3] = tempS[indexOfEmpty+3], tempS[indexOfEmpty]
-            #print(tempS)
-            if not(tempS in explored):
-                frontier.append(tempS)
-                #print(frontier)
-                explored.append(tempS)
-        if(indexOfEmpty - 1 >= bounds[0]):
-            #print("SLIDING LEFT")
-            tempS = list(currentS)
-            tempS[indexOfEmpty],tempS[indexOfEmpty-1] = tempS[indexOfEmpty-1], tempS[indexOfEmpty]
-            #print(tempS)
-            if not(tempS in explored):
-                frontier.append(tempS)
-                #print(frontier)
-                explored.append(tempS)
-        if(indexOfEmpty + 1 <= bounds[1]):
-            #print("SLIDING RIGHT")
-            tempS = list(currentS)
-            tempS[indexOfEmpty],tempS[indexOfEmpty+1] = tempS[indexOfEmpty+1], tempS[indexOfEmpty]
-            #print(tempS)
-            if not(tempS in explored):
-                frontier.append(tempS)
-                #print(frontier)
-                explored.append(tempS)
-        
-def boundaries(pos):
-    if (pos < 3):
-        return [0,2]
-    if (pos < 6):
-        return [3,5]
-    if (pos < 9):
-        return [6,8]
-
-def depth_first_search(state):
+        for nextChild in currentS.children:
+            if(nextChild):
+                nextState = state.State(nextChild)
+                if(not(nextState in explored)):
+                    frontier.append(nextState)
+                    explored.add(nextState)
+                
+def depth_first_search(stateInitial):
     frontier = collections.deque()
-    explored = collections.deque()
-    frontier.append(state)
-    nodesExpanded = -1
+    explored = set()
+    initstate = state.State(stateInitial)
+    frontier.append(initstate)
+    explored.add(initstate)
+    nodesExpanded = 0
     search_depth = 0
     max_search_depth = 0
     while(frontier):
         currentS = frontier.pop()
-        #print("State:",currentS)
+        #print("State:",currentS.currentState)
         #print("Frontier:",frontier)
-        if(currentS == [0,1,2,3,4,5,6,7,8]):
+        if(currentS.solved):
             print("Goal Found!")
             print("Nodes Expanded: ",nodesExpanded)
             return
         nodesExpanded += 1
-        indexOfEmpty = currentS.index(0)
-        #print(indexOfEmpty)
-        bounds = boundaries(indexOfEmpty)
-        #print(bounds)
-        if(indexOfEmpty + 1 <= bounds[1]):
-            #print("SLIDING RIGHT")
-            tempS = list(currentS)
-            tempS[indexOfEmpty],tempS[indexOfEmpty+1] = tempS[indexOfEmpty+1], tempS[indexOfEmpty]
-            #print(tempS)
-            if not(tempS in explored):
-                frontier.append(tempS)
-                #print(frontier)
-                explored.append(tempS)
-        if(indexOfEmpty - 1 >= bounds[0]):
-            #print("SLIDING LEFT")
-            tempS = list(currentS)
-            tempS[indexOfEmpty],tempS[indexOfEmpty-1] = tempS[indexOfEmpty-1], tempS[indexOfEmpty]
-            #print(tempS)
-            if not(tempS in explored):
-                frontier.append(tempS)
-                #print(frontier)
-                explored.append(tempS)
-        if(indexOfEmpty + 3 <= 8):
-            #print("SLIDING DOWN")
-            tempS = list(currentS)
-            tempS[indexOfEmpty],tempS[indexOfEmpty+3] = tempS[indexOfEmpty+3], tempS[indexOfEmpty]
-            #print(tempS)
-            if not(tempS in explored):
-                frontier.append(tempS)
-                #print(frontier)
-                explored.append(tempS)
-        if(indexOfEmpty - 3 >= 0):
-            #print("SLIDING UP")
-            tempS = list(currentS)
-            tempS[indexOfEmpty],tempS[indexOfEmpty-3] = tempS[indexOfEmpty-3], tempS[indexOfEmpty]
-            #print(tempS)
-            if not(tempS in explored):
-                frontier.append(tempS)
-                #print(frontier)
-                explored.append(tempS)
+        for nextChild in reversed(currentS.children):
+            if(nextChild):
+                nextState = state.State(nextChild)
+                if(not(nextState in explored)):
+                    frontier.append(nextState)
+                    explored.add(nextState)
         
         
 
--- state.py
+++ state.py
@@ -1,11 +1,63 @@
-class state:
+class State:
     def __init__(self, initialS):
-        self.goalState = ['0','1','2','3','4','5','6','7','8']
-        self.currentState(initialS)
-        self.cost = 100000
+        if initialS:
+            self.goalState = [0,1,2,3,4,5,6,7,8]
+            self.currentState = list(initialS)
+            self.solved = self.currentState == self.goalState
+            if not self.solved:
+                self.zeroLoc = self.currentState.index(0)
+                self.bounds = self.boundaries((self.zeroLoc))
+                self.children = [self.setUp(),self.setDown(),self.setLeft(self.bounds),self.setRight(self.bounds)]
+                self.cost = 100000
+    def __key(self):
+        return tuple(self.currentState)
+    def __hash__(self):
+        return hash(self.__key())
+    def __eq__(self,other):
+        return self.currentState == other.currentState
+    def __ne__(self,other):
+        return not (self.currentState == other.currentState)
 
-    def solved():
-
-    def heuristic():
-
+    def setUp(self):
+        if(self.zeroLoc - 3 >= 0):
+            #print("SLIDING UP")
+            #TODO Better representation of state to increase efficiency
+            tempS = list(self.currentState)
+            tempS[self.zeroLoc],tempS[self.zeroLoc-3] = tempS[self.zeroLoc-3], tempS[self.zeroLoc]
+            return tempS
+        else:
+            return None
+    def setDown(self):
+        if(self.zeroLoc + 3 <= 8):
+            #print("SLIDING DOWN")
+            tempS = list(self.currentState)
+            tempS[self.zeroLoc],tempS[self.zeroLoc+3] = tempS[self.zeroLoc+3], tempS[self.zeroLoc]
+            return tempS
+        else:
+            return None
     
+    def setLeft(self,bounds):
+        if(self.zeroLoc - 1 >= bounds[0]):
+            #print("SLIDING LEFT")
+            tempS = list(self.currentState)
+            tempS[self.zeroLoc],tempS[self.zeroLoc-1] = tempS[self.zeroLoc-1], tempS[self.zeroLoc]
+            return tempS
+        else:
+            return None
+
+    def setRight(self,bounds):
+        if(self.zeroLoc + 1 <= bounds[1]):
+            #print("SLIDING RIGHT")
+            tempS = list(self.currentState)
+            tempS[self.zeroLoc],tempS[self.zeroLoc+1] = tempS[self.zeroLoc+1], tempS[self.zeroLoc]
+            return tempS
+        else:
+            return None
+
+    def boundaries(self,zeroInd):
+        if (zeroInd < 3):
+            return [0,2]
+        if (zeroInd < 6):
+            return [3,5]
+        if (zeroInd < 9):
+            return [6,8]
